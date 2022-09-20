d95afad38408e3b6bfe0c79c87ad3f88fcac2165
Uditesh <uditesh24jha@gmail.com>
2022-09-20 00:07:35 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const profileImage = document.querySelector(".profile-img");
+var menu = document.getElementById("menu");
+var navLink = document.getElementsByClassName("nav-ul")[0];
+
+// on menu click toggle the class
+menu.onclick = function () {
+  navLink.classList.toggle("active");
+};
+
+// Mouse hover effect on profile image
+profileImage.addEventListener("mouseenter", () => {
+  profileImage.src = "../images/uj7.png";
+});
+profileImage.addEventListener("mouseleave", () => {
+  profileImage.src = "../images/uj6.png";
+});
+
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+mybutton.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});

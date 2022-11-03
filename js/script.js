d250261073e17cf807cd6810b23de14441d1518c
Uditesh <uditesh24jha@gmail.com>
2022-11-03 17:15:37 -0400

    new updates

--- js/script.js
+++ js/script.js
@@ -33,3 +33,50 @@ mybutton.addEventListener("click", () => {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 });
+
+// Contact Form
+var errorMsg = document.querySelector(".validation-error");
+function validateForm() {
+  if (document.contactForm.name.value == "") {
+    errorMsg.innerHTML = "Please enter your name.";
+    errorMsg.classList.add("active");
+
+    return false;
+  } else {
+    errorMsg.classList.remove("active");
+  }
+  var emailRegex = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
+  if (
+    document.contactForm.email.value == "" ||
+    !document.contactForm.email.value.match(emailRegex)
+  ) {
+    errorMsg.innerHTML = "Please enter a valid email address.";
+    errorMsg.classList.add("active");
+
+    return false;
+  } else {
+    errorMsg.classList.remove("active");
+  }
+  if (document.contactForm.message.value == "") {
+    errorMsg.innerHTML = "Please enter your message.";
+    errorMsg.classList.add("active");
+    return false;
+  } else {
+    errorMsg.classList.remove("active");
+  }
+  return true;
+}
+document.contactForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (validateForm()) {
+    document
+      .getElementsByClassName("success-submit-message")[0]
+      .classList.add("active");
+    document.contactForm.reset();
+    setTimeout(function () {
+      document
+        .getElementsByClassName("success-submit-message")[0]
+        .classList.remove("active");
+    }, 4000);
+  }
+});
---
+++ mail.php
@@ -0,0 +1,16 @@
+<?php
+if(isset($_POST['submit'])){
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$mailheader = "From:".$name."<".$email.">\r\n";
+
+$recipient =  "uditesh.r.jha@gmail.com";
+$subject = "Portfolio Message";
+
+mail($recipient,$subject,$message,$mailheader) or die("Error!");
+// header('Location: index.html');
+echo "Message sent";
+}
+?>
